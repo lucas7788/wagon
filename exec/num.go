a5d9113c3d4e61b756d962a0d8bc13226de0b149
lucas7788 <sishsh@163.com>
2021-09-02 12:20:22 +0800

    add log

--- exec/num.go
+++ exec/num.go
@@ -82,21 +82,40 @@ func (vm *VM) i32Shl() {
 	v2 := vm.popUint32()
 	v1 := vm.popUint32()
 	vm.pushUint32(v1 << (v2 % 32))
-	fmt.Println("i32Shl new:", v1<<(v2%32), "old:", v1<<v2)
+	new := v1 << (v2 % 32)
+	old := v1 << v2
+	if new != old {
+		fmt.Println("************i32Shl new:", new, "old:", old)
+	} else {
+		fmt.Println("i32Shl new:", new, "old:", old)
+	}
 }
 
 func (vm *VM) i32ShrU() {
 	v2 := vm.popUint32()
 	v1 := vm.popUint32()
 	vm.pushUint32(v1 >> (v2 % 32))
-	fmt.Println("i32ShrU new:", v1>>(v2%32), "old:", v1>>v2)
+	new := v1 >> (v2 % 32)
+	old := v1 >> v2
+	if new != old {
+		fmt.Println("************i32ShrU new:", new, "old:", old)
+	} else {
+		fmt.Println("i32ShrU new:", new, "old:", old)
+	}
+
 }
 
 func (vm *VM) i32ShrS() {
 	v2 := vm.popUint32()
 	v1 := vm.popInt32()
 	vm.pushInt32(v1 >> (v2 % 32))
-	fmt.Println("i32ShrS new:", v1>>(v2%32), "old:", v1>>v2)
+	new := v1 >> (v2 % 32)
+	old := v1 >> v2
+	if new != old {
+		fmt.Println("***********i32ShrS new:", new, "old:", old)
+	} else {
+		fmt.Println("i32ShrS new:", new, "old:", old)
+	}
 }
 
 func (vm *VM) i32Rotl() {
@@ -242,21 +261,41 @@ func (vm *VM) i64Shl() {
 	v2 := vm.popUint64()
 	v1 := vm.popUint64()
 	vm.pushUint64(v1 << (v2 % 64))
-	fmt.Println("i64Shl new:", v1<<(v2%64), "old:", v1<<v2)
+
+	new := v1 << (v2 % 64)
+	old := v1 << v2
+	if new != old {
+		fmt.Println("***********i64Shl new:", new, "old:", old)
+	} else {
+		fmt.Println("i64Shl new:", new, "old:", old)
+	}
 }
 
 func (vm *VM) i64ShrS() {
 	v2 := vm.popUint64()
 	v1 := vm.popInt64()
 	vm.pushInt64(v1 >> (v2 % 64))
-	fmt.Println("i64ShrS new:", v1>>(v2%64), "old:", v1>>v2)
+
+	new := v1 >> (v2 % 64)
+	old := v1 >> v2
+	if new != old {
+		fmt.Println("***********i64ShrS new:", new, "old:", old)
+	} else {
+		fmt.Println("i64ShrS new:", new, "old:", old)
+	}
 }
 
 func (vm *VM) i64ShrU() {
 	v2 := vm.popUint64()
 	v1 := vm.popUint64()
 	vm.pushUint64(v1 >> (v2 % 64))
-	fmt.Println("i64ShrU new:", v1>>(v2%64), "old:", v1>>v2)
+	new := v1 >> (v2 % 64)
+	old := v1 >> v2
+	if new != old {
+		fmt.Println("***********i64ShrU new:", new, "old:", old)
+	} else {
+		fmt.Println("i64ShrU new:", new, "old:", old)
+	}
 }
 
 func (vm *VM) i64Rotl() {
